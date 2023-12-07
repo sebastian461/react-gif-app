3e8eb0d87a9cf84bdc4b64777ff24193e8731866
sebastian461 <sebastiano-461@hotmail.com>
2023-12-07 11:59:52 -0500

    Test GifApp

---
+++ test/GifApp.test.jsx
@@ -0,0 +1,15 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { GifApp } from "../src/GifApp";
+
+describe("Pruebas en GifApp", () => {
+  test("debe retornar un arreglo de categorias", () => {
+    //TODO
+    /* const onAddCategory = jest.fn();
+    render(<GifApp />);
+    const input = screen.getByRole("textbox");
+    const form = screen.getByRole("form");
+    fireEvent.input(input, { target: { value: "Dragon Ball" } });
+    fireEvent.submit(form);
+    expect(onAddCategory).toHaveBeenCalled(); */
+  });
+});
