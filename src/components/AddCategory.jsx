696a98e1743c13a7beb1787b43d6b54066138079
sebastian461 <sebastiano-461@hotmail.com>
2023-12-07 10:51:49 -0500

    Test AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -32,7 +32,7 @@ export const AddCategory = ({ onNewCategory }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} aria-label="form">
       <input
         type="text"
         placeholder="Buscar Gifs"
